1a1ec852f05f3a86436884781fd17907756f486c
vieuxcolon <vieuxcolon@hotmail.fr>
2025-12-19 15:07:30 +0100

    Update dag_data_clean.py

--- dags/dag_data_clean.py
+++ dags/dag_data_clean.py
@@ -3,7 +3,8 @@
 # It uses utility functions defined in etl_utils.py to perform the various tasks of data cleaning and transformation
 # the resulting clean datasets include ariadb_clean, workaccidents_clean and fatalities_clean which are loaded into clean database tables
 # =====================================================================================================================================================
-# dag_data_fetch.py
+# dag_data_clean.py
+
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
