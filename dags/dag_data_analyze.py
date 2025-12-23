e1fe57aa5247bf744047edceeb30648e9fe5d732
vieuxcolon <vieuxcolon@hotmail.fr>
2025-12-23 20:55:37 +0100

    Update dag_data_analyze.py

--- dags/dag_data_analyze.py
+++ dags/dag_data_analyze.py
@@ -1,9 +1,10 @@
 # =================== dag_data_analyze.py ===================
+
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
-from etl_utils import create_dimensions_and_fact, min_test_star_schema, full_test_star_schema
+from etl_utils import create_star_schema, min_test_star_schema, full_test_star_schema
 
 with DAG(
     dag_id="dag_data_analyze",
@@ -15,7 +16,7 @@ with DAG(
 
     t1_star_schema = PythonOperator(
         task_id="create_star_schema",
-        python_callable=create_dimensions_and_fact
+        python_callable=create_star_schema
     )
 
     t2_min_test = PythonOperator(
