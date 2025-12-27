4fbe2d488174867ff51bd84fd5b0a852a3e20a3a
vieuxcolon <vieuxcolon@hotmail.fr>
2025-12-27 18:18:54 +0100

    Update dag_data_prep.py

--- dags/dag_data_prep.py
+++ dags/dag_data_prep.py
@@ -1,22 +1,15 @@
 # =================== dag_data_prep.py ===============================
 
-# =================== dag_data_prep.py ===================
-
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 
 from etl_utils import (
-    create_database_and_set_config,
     create_ariadb_prep,
     create_workaccidents_prep,
-    create_fatalities_prep,
-    create_star_schema
+    create_fatalities_prep
 )
-
-def create_project_db():
-    create_database_and_set_config("airflow")
 
 with DAG(
     dag_id="dag_data_prep",
@@ -26,11 +19,6 @@ with DAG(
     max_active_runs=1,
     tags=["prep"]
 ) as dag:
-
-    t0 = PythonOperator(
-        task_id="create_db",
-        python_callable=create_project_db
-    )
 
     t1 = PythonOperator(
         task_id="create_ariadb_prep",
@@ -47,11 +35,6 @@ with DAG(
         python_callable=create_fatalities_prep
     )
 
-    t4 = PythonOperator(
-        task_id="create_star_schema",
-        python_callable=create_star_schema
-    )
-
     trigger_analyze = TriggerDagRunOperator(
         task_id="trigger_data_analyze",
         trigger_dag_id="dag_data_analyze",
@@ -60,4 +43,4 @@ with DAG(
         failed_states=["failed"]
     )
 
-    t0 >> [t1, t2, t3] >> t4 >> trigger_analyze
+    [t1, t2, t3] >> trigger_analyze
