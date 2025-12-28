953fceb3050d15604350e980c28c3bf2caedd0ce
vieuxcolon <vieuxcolon@hotmail.fr>
2025-12-28 01:55:30 +0100

    Update dag_data_clean.py

--- dags/dag_data_clean.py
+++ dags/dag_data_clean.py
@@ -57,7 +57,7 @@ with DAG(
     # ----------------------------
     trigger_prep = TriggerDagRunOperator(
         task_id="trigger_data_prep",
-        trigger_dag_id="dag_data_cprep",
+        trigger_dag_id="dag_data_dprep",
         wait_for_completion=True,
         allowed_states=["success"],
         failed_states=["failed"]
