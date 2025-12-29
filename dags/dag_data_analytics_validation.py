2d3f0a6d85c5a54d790cfb882e059ae3971601ae
vieuxcolon <vieuxcolon@hotmail.fr>
2025-12-29 11:05:47 +0100

    Update dag_data_analytics_validation.py

--- dags/dag_data_analytics_validation.py
+++ dags/dag_data_analytics_validation.py
@@ -1,4 +1,9 @@
-# =================== dag_data_analytics_validation.py ===============================
+# =================== dag_data_analytics_validation.py ================================================================================================
+# It uses utility functions defined the run_analytics_validation of etl_utils.py to execute the following 9 analytical queries:
+# 1. Fatalities by Year, 2. Fatalities by Country, 3. Fatalities by Industry, 4. Evolution of fatalities France vs USA (last decade),
+# 5. Top 10 countries by fatalities last year, 6. Top 10 industries by fatalities last year, 7. Countries with zero fatalities last year (top 10)
+# 8. Fatalities trend by top 5 countries (last 5 years), 9. Fatalities per country per month (last year, top 10 records) 
+# =====================================================================================================================================================
 
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
